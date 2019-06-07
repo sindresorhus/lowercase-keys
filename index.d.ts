804805fc71e1ccf9c47907ad1a3881376f389c1d
Sindre Sorhus <sindresorhus@gmail.com>
2019-06-07 14:58:24 +0700

    Minor TypeScript definition tweak

--- index.d.ts
+++ index.d.ts
@@ -11,6 +11,6 @@ lowercaseKeys({FOO: true, bAr: false});
 //=> {foo: true, bar: false}
 ```
 */
-declare function lowercaseKeys<T extends unknown>(object: {[key: string]: T}): {[key: string]: T};
+declare function lowercaseKeys<T>(object: {[key: string]: T}): {[key: string]: T};
 
 export = lowercaseKeys;
