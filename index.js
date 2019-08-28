f7e53da4c7603e23c4129e3311b3c41964bc4757
yiming.hsu <yiming.hsu@cowell.com.tw>
2019-08-28 13:08:14 +0800

    deep lowercase

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict';
-module.exports = object => {
+module.exports = function lowercaseKeys(object) {
+	if (object === null) {
+		return null;
+	}
+
 	const result = {};
 
 	for (const [key, value] of Object.entries(object)) {
-		result[key.toLowerCase()] = value;
+		if (Array.isArray(value)) {
+			result[key.toLowerCase()] = [];
+			for (const v of value) {
+				result[key.toLowerCase()].push(lowercaseKeys(v));
+			}
+		} else {
+			result[key.toLowerCase()] = (typeof value === 'object') ? lowercaseKeys(value) : value;
+		}
 	}
 
 	return result;
--- test.js
+++ test.js
@@ -4,4 +4,18 @@ import lowercaseKeys from '.';
 test('main', t => {
 	t.true(lowercaseKeys({FOO: true}).foo);
 	t.true(lowercaseKeys({FOO: true, bAr: true}).bar);
+	t.deepEqual(lowercaseKeys({Profile: {Gender: 'Male', MySkill: [{Name: 'skill-1', Years: 5}, {Name: 'skill-2', Years: 17}]}}).profile,
+		{
+			gender: 'Male',
+			myskill: [
+				{
+					name: 'skill-1',
+					years: 5
+				},
+				{
+					name: 'skill-2',
+					years: 17
+				}
+			]
+		});
 });
