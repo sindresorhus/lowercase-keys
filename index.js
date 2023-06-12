97bae3f82e01501a4e29a1c3edcac31445920102
Rahul Sharma <rahu8299@gmail.com>
2023-06-12 18:56:03 +0530

    improving performance

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 export default function lowercaseKeys(object) {
-	return Object.fromEntries(Object.entries(object).map(([key, value]) => [key.toLowerCase(), value]));
+	const objCopy = {};
+	const objList = Object.entries(object);
+	for (let index = 0; index < objList.length; index++) {
+		objCopy[objList[index][0].toLowerCase()] = objList[index][1];
+	}
+	return objCopy;
 }
